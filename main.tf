8c9feaf73455f73f3eef2c94623277542a54985f
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-10-21 15:31:13 +0200

    ci: add release workflow and prepare main to get version from txt

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "argocd_application" "this" {
     source {
       repo_url        = "https://github.com/camptocamp/devops-stack-module-thanos.git"
       path            = "charts/thanos"
-      target_revision = "main"
+      target_revision = trimspace(file("${path.module}/version.txt"))
       helm {
         values = data.utils_deep_merge_yaml.values.output
       }
