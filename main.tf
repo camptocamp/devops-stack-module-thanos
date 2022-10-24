dea575b5475a830c1afd0a43c1176d359f7255d1
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-10-24 09:23:07 +0200

    test: point temporarily to chart-upgrade

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "argocd_application" "this" {
     source {
       repo_url        = "https://github.com/camptocamp/devops-stack-module-thanos.git"
       path            = "charts/thanos"
-      target_revision = "main"
+      target_revision = "chart-upgrade" # TODO remove this and point to version.txt
       helm {
         values = data.utils_deep_merge_yaml.values.output
       }
