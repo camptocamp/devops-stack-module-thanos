2b4c257537813e355fed9866e07e74fe19964057
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-11-21 12:13:32 +0100

    feat(aks): add simplified aks variant with bucket conf interface

---
+++ aks/local.tf
@@ -0,0 +1,31 @@
+locals {
+  # values.yaml translated into HCL structures.
+  # Possible values available here -> https://github.com/bitnami/charts/tree/master/bitnami/thanos/
+  helm_values = [{
+    thanos = {
+
+      objstoreConfig = {
+        type = "AZURE"
+        config = {
+          storage_account     = "${local.thanos.metrics_storage.storage_account}"
+          storage_account_key = "${local.thanos.metrics_storage.storage_account_key}"
+          container           = "${local.thanos.metrics_storage.container}"
+        }
+      }
+
+    }
+  }]
+
+  thanos_defaults = {
+    metrics_storage = {
+      storage_account     = null
+      storage_account_key = null
+      container           = null
+    }
+  }
+
+  thanos = merge(
+    local.thanos_defaults,
+    var.thanos,
+  )
+}
