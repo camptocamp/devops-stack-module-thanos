6ebef7a0d20073f9e0afd251a49087cab4e7bbf5
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-12-16 11:49:08 +0100

    fix: change values names to the same as in kube-prometheus-stack module

--- aks/extra-variables.tf
+++ aks/extra-variables.tf
@@ -1,8 +1,8 @@
 variable "metrics_storage" {
   description = "Azure Blob Storage configuration values for the storage container where the archived metrics will be stored."
   type = object({
-    container_name       = string
-    storage_account_name = string
-    storage_account_key  = string
+    container           = string
+    storage_account     = string
+    storage_account_key = string
   })
 }
--- aks/locals.tf
+++ aks/locals.tf
@@ -4,8 +4,8 @@ locals {
       objstoreConfig = {
         type = "AZURE"
         config = {
-          container           = "${var.metrics_storage.container_name}"
-          storage_account     = "${var.metrics_storage.storage_account_name}"
+          container           = "${var.metrics_storage.container}"
+          storage_account     = "${var.metrics_storage.storage_account}"
           storage_account_key = "${var.metrics_storage.storage_account_key}"
         }
       }
