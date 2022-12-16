8ac533353e945bb6a20d1921af922b2c45e418c8
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-12-16 13:19:53 +0100

    feat: add variable to configure sync on the application resource

--- variables.tf
+++ variables.tf
@@ -41,6 +41,20 @@ variable "helm_values" {
   default     = []
 }
 
+variable "app_autosync" {
+  description = "Automated sync options for the Argo CD Application resource."
+  type = object({
+    allow_empty = optional(bool)
+    prune       = optional(bool)
+    self_heal   = optional(bool)
+  })
+  default = {
+    allow_empty = false
+    prune       = true
+    self_heal   = true
+  }
+}
+
 variable "dependency_ids" {
   description = "IDs of the other modules on which this module depends on."
   type        = map(string)
