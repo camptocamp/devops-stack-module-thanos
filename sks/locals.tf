6c76ee20c8225ee16dafd45cfc0e523b76cd0268
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-06-12 11:32:50 +0200

    refactor: standardize variables like in other modules

--- sks/locals.tf
+++ sks/locals.tf
@@ -6,9 +6,9 @@ locals {
         type = "S3"
         config = {
           bucket     = "${var.metrics_storage.bucket_name}"
-          endpoint   = "sos-${var.metrics_storage.bucket_region}.exo.io"
+          endpoint   = "sos-${var.metrics_storage.region}.exo.io"
           access_key = "${var.metrics_storage.access_key}"
-          secret_key = "${var.metrics_storage.secret_access_key}"
+          secret_key = "${var.metrics_storage.secret_key}"
         }
       }
     }
