30a9f81573c53621c3c867878fcde1b91e276d9b
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-06-12 11:32:50 +0200

    refactor: standardize variables like in other modules

--- sks/extra-variables.tf
+++ sks/extra-variables.tf
@@ -6,9 +6,9 @@ variable "cluster_id" {
 variable "metrics_storage" {
   description = "Exoscale SOS bucket configuration values for the bucket where the archived metrics will be stored."
   type = object({
-    bucket_name       = string
-    bucket_region     = string
-    access_key        = string
-    secret_access_key = string
+    bucket_name = string
+    region      = string
+    access_key  = string
+    secret_key  = string
   })
 }
