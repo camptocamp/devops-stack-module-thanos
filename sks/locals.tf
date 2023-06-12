fa49504472e0f4cd6e1aa17639d73904069ebb06
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-06-12 09:48:09 +0200

    feat: add first version of the SKS variant

---
+++ sks/locals.tf
@@ -0,0 +1,16 @@
+locals {
+  helm_values = [{
+    thanos = {
+      clusterDomain = "${var.cluster_id}.cluster.local"
+      objstoreConfig = {
+        type = "S3"
+        config = {
+          bucket     = "${var.metrics_storage.bucket_name}"
+          endpoint   = "sos-${var.metrics_storage.bucket_region}.exo.io"
+          access_key = "${var.metrics_storage.access_key}"
+          secret_key = "${var.metrics_storage.secret_access_key}"
+        }
+      }
+    }
+  }]
+}
