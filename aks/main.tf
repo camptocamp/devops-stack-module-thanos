3dcfb17f2faac830480c304a1308a1860e309183
Osman Mohammed <osman.mohammed@camptocamp.com>
2023-09-14 16:09:47 +0200

    feat(thanos): add deep_merge_append_list variable

--- aks/main.tf
+++ aks/main.tf
@@ -30,14 +30,15 @@ resource "azurerm_role_assignment" "contributor" {
 module "thanos" {
   source = "../"
 
-  cluster_name     = var.cluster_name
-  base_domain      = var.base_domain
-  argocd_namespace = var.argocd_namespace
-  target_revision  = var.target_revision
-  cluster_issuer   = var.cluster_issuer
-  namespace        = var.namespace
-  app_autosync     = var.app_autosync
-  dependency_ids   = var.dependency_ids
+  cluster_name           = var.cluster_name
+  base_domain            = var.base_domain
+  argocd_namespace       = var.argocd_namespace
+  target_revision        = var.target_revision
+  cluster_issuer         = var.cluster_issuer
+  namespace              = var.namespace
+  deep_merge_append_list = var.deep_merge_append_list
+  app_autosync           = var.app_autosync
+  dependency_ids         = var.dependency_ids
 
   thanos = var.thanos
 
