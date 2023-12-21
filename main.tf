b242859cbb0fa6d9c1a25958dd074335290f3d64
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-12-21 20:04:45 +0100

    fix!: remove the namespace variable
    We found out that on multiple modules we were referencing resources from other modules using their default namespaces and this was hardcoded. In case someone decided to change the namespace of deployment of a certain application, this could break the way modules work with each other.
    
    We've decided that in order to avoid undefined behaviors caused by overloading this variable, it would be best to remove it entirely.

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "argocd_project" "this" {
 
     destination {
       name      = var.destination_cluster
-      namespace = var.namespace
+      namespace = "thanos"
     }
 
     orphaned_resources {
@@ -75,7 +75,7 @@ resource "argocd_application" "this" {
 
     destination {
       name      = var.destination_cluster
-      namespace = var.namespace
+      namespace = "thanos"
     }
 
     sync_policy {
